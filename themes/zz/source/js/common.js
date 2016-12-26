93e40078eb0393f159ccce6e0ee6b076523cb146
Weilin <934587911@qq.com>
2016-12-26 14:12:03 +0800

    basic translation done

--- themes/zz/source/js/common.js
+++ themes/zz/source/js/common.js
@@ -3,4 +3,12 @@
     speed: 400,
     offset: 0
   });
+  // drop down
+  var dropdowns = document.querySelectorAll('.dropdown-toggle');
+  dropdowns = Array.prototype.slice.call(dropdowns);
+  dropdowns.forEach(function (button) {
+    button.addEventListener('click', function () {
+      button.parentElement.classList.toggle('open');
+    });
+  });
 })();
