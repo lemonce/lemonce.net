374b54af935c05c95f2d11a2f52f9b3e0c4f631f
Weilin <934587911@qq.com>
2016-12-26 16:07:25 +0800

    code highlight

--- themes/zz/source/js/common.js
+++ themes/zz/source/js/common.js
@@ -3,12 +3,5 @@
     speed: 400,
     offset: 0
   });
-  // drop down
-  var dropdowns = document.querySelectorAll('.dropdown-toggle');
-  dropdowns = Array.prototype.slice.call(dropdowns);
-  dropdowns.forEach(function (button) {
-    button.addEventListener('click', function () {
-      button.parentElement.classList.toggle('open');
-    });
-  });
+  sublemon.init();
 })();
