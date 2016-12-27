2774e808acfaf7a4a034e75960519ff9c852c90f
Weilin <934587911@qq.com>
2016-12-27 18:10:07 +0800

    [ADDED] keyword template [FIXED] call to action button color

---
+++ scripts/helpers.js
@@ -0,0 +1,23 @@
+'use strict';
+
+hexo.extend.helper.register('url_for_lang', function(path){
+  // lang will be en or zh-cn, etc.
+  var lang = this.page.lang;
+  // path will be normalized
+  var url = this.url_for(path);
+
+  if (lang !== 'en' && url[0] === '/') url = '/' + lang + url;
+
+  return url;
+});
+
+hexo.extend.helper.register('title_for', function () {
+  var pageTitle = this.page.title;
+  var siteTitle = this.config.title;
+
+  if (pageTitle) {
+    return pageTitle + ' - ' + siteTitle;
+  }
+
+  return siteTitle;
+});
