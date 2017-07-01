2a7787d997881eff00f078fb3d2b1ba1da6d3fb0
Weilin Shi <934587911@qq.com>
2017-07-01 11:34:25 +0800

    [ADDED] ga for all download and license links

--- themes/zz/source/js/common.js
+++ themes/zz/source/js/common.js
@@ -4,4 +4,23 @@
     offset: 0
   });
   sublemon.init();
+  // bind all link
+  var links = document.querySelectorAll('a[href]');
+  for (var i = 0; i < links.length; i++) {
+    var el = links[i];
+    bindGA(el);
+  }
+  /** @param {HTMLElement} el */
+  function bindGA(el) {
+    if (typeof el.onclick === 'function') {
+      return;
+    }
+    if (/license/i.test(el.href)) {
+      el.onclick = function () {
+        ga('send', 'event', 'click', 'license', 'md link');
+      }
+    } else {
+      // Do nothing for now
+    }
+  }
 })();
